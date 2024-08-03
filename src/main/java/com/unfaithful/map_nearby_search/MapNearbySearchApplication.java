fafab3e3e2f872a58a54b841a052032f8ddcc0d6
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2024-08-03 12:03:20 +0300

    Initialized map-nearby-search project.

---
+++ src/main/java/com/unfaithful/map_nearby_search/MapNearbySearchApplication.java
@@ -0,0 +1,14 @@
+package com.unfaithful.map_nearby_search;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MapNearbySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapNearbySearchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/unfaithful/map_nearby_search/MapNearbySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unfaithful.map_nearby_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapNearbySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
